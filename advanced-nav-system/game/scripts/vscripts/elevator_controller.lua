65e656e14f67847675ad712ca5e5a2bc53eb664d
minhcongton <minhct@yogame.vn>
2023-09-14 10:18:08 +0700

    Clean up lua code

--- advanced-nav-system/game/scripts/vscripts/elevator_controller.lua
+++
@@ -1,8 +0,0 @@
-function move_elevator_up()
-    print("Elevator start running...")
-    if thisEntity == nil then
-        print("Current entity is nil")
-        return
-    end
-    thisEntity:SetVelocity(Vector(0, 0, 10))
-end
--- advanced-nav-system/game/scripts/vscripts/moc_testing.lua
+++
@@ -1,5 +0,0 @@
-function ChangeAlpha()
-    print("MOC test current entity index: " .. thisEntity:GetEntityIndex())
-    print("MOC test current entity index: " .. thisEntity:entindex())
-    thisEntity:SetRenderAlpha(15)
-end
